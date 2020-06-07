621632aae83939401f21c81c48a8c5c3a1912a69
edmundo papi <edmundopierre@yahoo.com>
2020-06-07 09:06:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,6 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:alan_kay][:known_for] = alan_kay_is_known_for
+  programmer_hash[:alan_kay][:languages] = alan_kay_is_known_for
   programmer_hash
 end
